eca4002c2ff675091f68206d7b2c05c0e979c6c8
kevinzxc1217 <kevinzxc1217@gmail.com>
2024-03-15 13:13:34 +0800

    Implement "q_remove_head", "q_remove_tail"
    Implement q_remove_head and q_remove_tail, first we need to check head or list exist, second copy value to the sp, final delete head or tail node.

--- queue.c
+++ queue.c
@@ -78,13 +78,29 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if(!head || list_empty(head))
+        return NULL;
+    
+    element_t *first = list_first_entry(head, element_t, list);
+    strncpy(sp, first -> value, bufsize -1);
+    sp[bufsize - 1] = '\0';
+    list_del_init(&(first -> list));
+    
+    return first;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if(!head || list_empty(head))
+        return NULL;
+        
+    element_t *last = list_last_entry(head, element_t, list);    
+    strcpy(sp,last->value);
+    sp[bufsize - 1] = '\0';
+    list_del_init(&(last -> list));
+    
+    return last;
 }
 
 /* Return number of elements in queue */
