921351a1e4c55007bfcdb1e1dafa4732ddc673b4
kevinzxc1217 <kevinzxc1217@gmail.com>
2024-03-20 22:11:22 +0800

    Update "list_sort.h"
    Apply coding convention to the changes.

--- list_sort.h
+++ list_sort.h
@@ -6,9 +6,12 @@
 
 struct list_head;
 
-typedef int __attribute__((nonnull(2,3))) (*list_cmp_func_t)(void *,
-		const struct list_head *, const struct list_head *);
+typedef int
+    __attribute__((nonnull(2, 3))) (*list_cmp_func_t)(void *,
+                                                      const struct list_head *,
+                                                      const struct list_head *);
 
-__attribute__((nonnull(2,3)))
-void list_sort(void *priv, struct list_head *head, list_cmp_func_t cmp);
+__attribute__((nonnull(2, 3))) void list_sort(void *priv,
+                                              struct list_head *head,
+                                              list_cmp_func_t cmp);
 #endif
